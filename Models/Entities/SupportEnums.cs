d1d82b58ec2e638e00e26ed82f30631f91116f53
Giovanni Sinobolli <92578145+GiovanniSinobolli@users.noreply.github.com>
2025-08-09 14:35:40 -0400

    Delete Models directory

--- Models/Entities/SupportEnums.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SleepSync.Models.Entities
-{
-    public class SupportEnums
-    {
-        public enum Priority { Low = 1, Medium = 2, High = 3 }
-        public enum StatusTask { Pending, InProgress, Completed, Cancelled }
-        public enum MoodLevel { Terrible = 1, Bad = 2, Neutral = 3, Happy = 4 }
-    }
-}
